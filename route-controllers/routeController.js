5f3336f8b5403e13231d028ac769b2b65a94f5b7
Loh Chun Mun <chunmun_loh@live.com>
2022-12-04 04:12:15 +0800

    added dynamic breadcrumbs to all existing pages

--- route-controllers/routeController.js
+++ route-controllers/routeController.js
@@ -2,36 +2,42 @@ const HTTP_STATUS = require("../constants/http_status");
 
 exports.viewIndex = async (req, res, next) => {
     res.status(HTTP_STATUS.OK).render('index', {
+        pageTitle: 'Index',
         path: '/'
     });
 };
 
 exports.viewLogin = async (req, res, next) => {
     res.status(HTTP_STATUS.OK).render('auth/login', {
+        pageTitle: 'Login',
         path: '/login'
     });
 };
 
 exports.viewRegister = async (req, res, next) => {
     res.status(HTTP_STATUS.OK).render('auth/register', {
+        pageTitle: 'Register',
         path: '/register'
     });
 };
 
 exports.viewForgotPassword = async (req, res, next) => {
     res.status(HTTP_STATUS.OK).render('auth/forgot-password', {
+        pageTitle: 'Forget Password',
         path: '/forgot-password'
     });
 };
 
 exports.viewInvoice = async (req, res, next) => {
     res.status(HTTP_STATUS.OK).render('bill/invoice', {
+        pageTitle: 'Invoice',
         path: '/invoice'
     });
 };
 
 exports.viewInvoicePrint = async (req, res, next) => {
     res.status(HTTP_STATUS.OK).render('bill/invoice-print', {
+        pageTitle: 'Invoice',
         path: '/invoice-print'
     });
 };
