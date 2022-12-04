7fe9e44b4dd2189025750407a95441b45d9133bc
Loh Chun Mun <chunmun_loh@live.com>
2022-12-04 19:22:50 +0800

    added `view all` appointments for admin

--- route-controllers/routeController-admin.js
+++ route-controllers/routeController-admin.js
@@ -1,4 +1,6 @@
+const _ = require('lodash');
 const request = require('request');
+const moment = require('moment');
 const parse_uri = require("../lib/parse_uri");
 const HTTP_STATUS = require("../constants/http_status");
 
@@ -61,9 +63,31 @@ exports.createPatient = async (req, res, next) => {
 };
 
 exports.viewAppointments = async (req, res, next) => {
-    res.status(HTTP_STATUS.OK).render('table/admin-appointments', {
-        pageTitle: 'Appointment',
-        path: '/admin/appointment/view-all'
+    // api endpoint uri
+    const uri = parse_uri.parse(req, '/api/appointment/get/all');
+    request.get({
+        url: uri,
+    }, (err, response, body) => {
+        if (response.statusCode === HTTP_STATUS.OK) {
+            let data = JSON.parse(response.body);
+
+            data = _.map(data, (appt) => {
+                appt.apptDateTime = moment(new Date(appt.apptDateTime)).format('DD/MM/YYYY hh:mmA');
+                return appt;
+            });
+
+            res.status(HTTP_STATUS.OK).render('table/appointments', {
+                pageTitle: 'Appointment',
+                path: '/admin/appointment/view-all',
+                appointmentData: data
+            });
+        }
+        else {
+            res.status(HTTP_STATUS.NOT_FOUND).render('404', {
+                pageTitle: 'Appointment',
+                path: '/admin/appointment/view-all'
+            });
+        }
     });
 };
 
