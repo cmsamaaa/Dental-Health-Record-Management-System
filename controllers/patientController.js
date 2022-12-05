6afaf3378233396004407bd488d9f85e7997a764
Loh Chun Mun <chunmun_loh@live.com>
2022-12-05 17:10:43 +0800

    changed default login to patient login only

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -1,10 +1,12 @@
 const _ = require('lodash');
 const bcrypt = require('bcryptjs');
+const request = require('request');
 
 const Patient = require('../entities/patient');
 const parse_uri = require("../lib/parse_uri");
+const HTTP_STATUS = require("../constants/http_status");
 
-exports.registerPatient = async (req, res, next) => {
+exports.register = async (req, res, next) => {
     const hashedPassword = await bcrypt.hash(req.body.nric, 12); // encrypt password with bcrypt, salt length 12
     if (!_.isEmpty(req.body)) {
         const patient = new Patient({
@@ -30,4 +32,37 @@ exports.registerPatient = async (req, res, next) => {
     }
     else
         res.redirect(parse_uri.parse(req, '/admin/patient/create?error=true'));
+};
+
+exports.login = async (req, res, next) => {
+    if (!_.isEmpty(req.body)) {
+        const patient = new Patient(req.body);
+        const result = await patient.authenticatePatient();
+
+        if (!_.isEmpty(result)) {
+            req.session.isLoggedIn = true;
+            req.session.userId = result.userId;
+
+            request.get({
+                url: parse_uri.parse(req, '/api/info/get/all'),
+            }, (err, response, body) => {
+                if (response.statusCode === HTTP_STATUS.OK) {
+                    const clinicInfo = JSON.parse(body);
+                    const infoDict = {};
+                    for (let i = 0; i < clinicInfo.length; i++) {
+                        infoDict[clinicInfo[i].infoKey] = clinicInfo[i].value;
+                    }
+
+                    req.session.clinicInfo = infoDict;
+                    res.redirect(parse_uri.parse(req, '/index?result=true&id=' + req.session.userId));
+                }
+                else
+                    res.redirect(parse_uri.parse(req, '/login?error=true'));
+            });
+        }
+        else
+            res.redirect(parse_uri.parse(req, '/login?error=true'));
+    }
+    else
+        res.redirect(parse_uri.parse(req, '/login?error=true'));
 };
--- routes/backend.js
+++ routes/backend.js
@@ -4,18 +4,21 @@ const userController = require('../controllers/userController');
 const clinicInfoController = require('../controllers/clinicInfoController');
 const patientController = require('../controllers/patientController');
 const appointmentController = require('../controllers/appointmentController');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
+// Non-protected routes
+router.post('/login', patientController.login);
 router.post('/register', userController.register);
-router.post('/login', userController.login);
 router.get('/logout', userController.logout);
 
-router.post('/admin/clinic/add-information', clinicInfoController.addClinicInfo);
+// Protected routes
+router.post('/admin/clinic/add-information', isAuth, clinicInfoController.addClinicInfo);
 
-router.post('/admin/patient/create', patientController.registerPatient);
+router.post('/admin/patient/create', isAuth, patientController.register);
 
-router.post('/admin/appointment/create', appointmentController.createAppointment);
-router.post('/patient/appointment/create', appointmentController.createAppointment);
+router.post('/admin/appointment/create', isAuth, appointmentController.createAppointment);
+router.post('/patient/appointment/create', isAuth, appointmentController.createAppointment);
 
 module.exports = router;
