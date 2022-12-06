0b34e2afcd6c4b8e4ffb8f2a4bd89be57daccb15
Loh Chun Mun <chunmun_loh@live.com>
2022-12-07 03:10:23 +0800

    fix logout redirect logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,9 @@
-const _ = require('lodash');
-const bcrypt = require('bcryptjs');
-const request = require('request');
-
 const User = require('../entities/user');
 const parse_uri = require("../lib/parse_uri");
-const HTTP_STATUS = require("../constants/http_status");
 
 exports.logout = async (req, res, next) => {
     let path = '/login?logout=true';
-    if (req.session.userRole !== 'Patient')
+    if (req.session.userRole === 'Administrator' || req.session.userRole === 'Dentist' || req.session.userRole === 'Dental Assistant')
         path = '/staff' + path;
 
     req.session.destroy();
