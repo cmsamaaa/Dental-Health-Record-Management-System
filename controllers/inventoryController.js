c757b8ea3f8cb1c0c6bbf2d9b8af81c0f8ce0594
linkaiqi <linnnkaiqi@gmail.com>
2022-12-28 18:02:16 +0800

    added inventory

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -4,7 +4,7 @@ const Inventory = require('../entities/inventory');
 const parse_uri = require("../lib/parse_uri");
 const HTTP_STATUS = require("../constants/http_status");
 
-exports.viewInventory = async (req, res, next) => {
+exports.viewInventories = async (req, res, next) => {
     res.status(HTTP_STATUS.OK).render('table/admin-inventory', {
         pageTitle: 'Inventory',
         path: '/admin/inventory/view-all',
@@ -33,6 +33,51 @@ exports.createInventory = async (req, res, next) => {
     }
     else
         res.redirect(parse_uri.parse(req, '/admin/inventory/create?error=true'));
+};
+exports.editInventory = async (req, res, next) => {
+    // api endpoint uri
+    const uri = parse_uri.parse(req, '/admin/inventory/edit');
+    request.post({
+        url: uri,
+        form: req.body
+    }, (err, response, body) => {
+        if (response.statusCode === HTTP_STATUS.OK)
+            res.redirect(parse_uri.parse(req, '/' + req.body.for + '/inventory/view-all?action=edit&id=' + req.body.inventoryId));
+        else
+            res.redirect(parse_uri.parse(req, '/' + req.body.for + '/inventory/view-all?error=true'));
+    });
+};
+
+exports.suspendInventory = async (req, res, next) => {
+    // api endpoint uri
+    const uri = parse_uri.parse(req, '/admin/inventory/suspend');
+    request.post({
+        url: uri,
+        form: req.body
+    }, (err, response, body) => {
+        if (response.statusCode === HTTP_STATUS.OK)
+            res.redirect(parse_uri.parse(req, '/' + req.body.for + '/inventory/view-all?action=cancel&id=' + req.body.inventoryId));
+        else
+            res.redirect(parse_uri.parse(req, '/' + req.body.for + '/inventory/view-all?error=true'));
+    });
+};
+
+exports.viewInventory = async (req, res, next) => {
+    const inventory = new Inventory({
+        inventoryId: req.params.inventoryId
+    });
+    const result = await inventory.getInventory();
+
+    if (!_.isEmpty(result)) {
+        res.status(HTTP_STATUS.OK).render('detail/inventory', {
+            pageTitle: 'Inventory',
+            path: '/admin/inventory/view',
+            query: req.query,
+            inventoryData: result
+        });
+    }
+    else
+        res.redirect(parse_uri.parse(req, '/admin/inventory/view-all?error=true'));
 };
 
 exports.viewCreateInventory = async (req, res, next) => {
