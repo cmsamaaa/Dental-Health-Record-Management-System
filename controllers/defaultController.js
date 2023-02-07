c1be17b1a7fe4bc781e565997cd71fea98885c2b
linkaiqi <linnnkaiqi@gmail.com>
2023-02-07 19:59:16 +0800

    updated homepage design and infos

--- controllers/defaultController.js
+++ controllers/defaultController.js
@@ -1,10 +1,17 @@
 const HTTP_STATUS = require("../constants/http_status");
+const Clinic = require('../entities/clinic');
 
 exports.viewIndex = async (req, res, next) => {
+    const clinic = new Clinic({
+        //clinicId: req.session.userInfo.clinicId
+    });
+    const result = await clinic.getAll();
+
     res.status(HTTP_STATUS.OK).render('index', {
         pageTitle: 'Index',
         path: '/',
         query: req.query,
+        clinicsData: result
     });
 };
 
